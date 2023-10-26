6e81aff6a281b34d17b065fd276f53d0b02b8279
Lazerlight <snndsnns2@gmail.com>
2023-10-26 17:26:03 +0300

    save

--- script.js
+++ script.js
@@ -54,8 +54,44 @@ const getBet = (balance, lines) => {
   }
 };
 
-const spin = () => {};
+const spin = () => {
+  const symbols = [];
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+
+  const reels = [];
+  for (let i = 0; i < COLS; i++) {
+    reels.push([]);
+    const reelSymbols = [...symbols];
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
+    }
+  }
+  return reels;
+};
+
+const transpose = (reels) => {
+  const rows = [];
+
+  for (let i = 0; i < ROWS; i++) {
+    rows.push([]);
+    for (let j = 0; j < COLS; j++) {
+      rows[i].push(reels[j][i]);
+    }
+  }
+  return rows;
+};
 
 let balance = deposit();
 const linesCount = getLinesCount();
 const bet = getBet(balance, linesCount);
+const reels = spin();
+const rows = transpose(reels);
+console.log(reels);
+console.log(rows);
